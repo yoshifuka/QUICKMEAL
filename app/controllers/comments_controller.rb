bb365daf9082021ca57d3d55b928ab3b7492b34b
yoshifuka <wombat0707@gmail.com>
2021-10-09 12:31:08 +0900

    不要変数削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @dish = Dish.find(params[:dish_id])
-    @user = @dish.user
     @comment = @dish.comments.build(user_id: current_user.id, content: params[:comment][:content])
     if !@dish.nil? && @comment.save
       flash[:success] = "コメントを追加しました！"
     else
       flash[:danger] = "空のコメントは投稿できません。"
     end
-    redirect_to request.referrer || root_path
+    redirect_to dish_path(@dish)
   end
 
   def destroy
@@ -16,7 +15,9 @@ class CommentsController < ApplicationController
     @dish = @comment.dish
     if current_user.id == @comment.user_id
       @comment.destroy
-      flash[:success] = "コメントを削除しました"
+      flash[:success] = "コメントを削除しました。"
+    else
+      flash[:danger] = "コメントを削除できません。"
     end
     redirect_to dish_path(@dish)
   end
