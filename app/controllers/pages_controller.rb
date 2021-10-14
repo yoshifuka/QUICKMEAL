659eabb4234710a5efb6ad2bae94b8f61baf3312
yoshifuka <wombat0707@gmail.com>
2021-10-14 22:37:45 +0900

    フォロー機能

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
-     end
-   end
-
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(show following followers)
+
   def show
-    @user = User.find_by(id: params[:id])
     @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)
+  end
+
+  def following
+    @title = "フォロー中"
+    @users = @user.following.paginate(page: params[:page], per_page: 5)
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @users = @user.followers.paginate(page: params[:page], per_page: 5)
+    render 'show_follow'
+  end
+
+  private
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
