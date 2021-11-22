ceb85fc2aca189a1b55f28854b2daf9651e02048
yoshifuka <wombat0707@gmail.com>
2021-11-22 14:28:01 +0900

    リスト機能

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -11,7 +11,7 @@ class DishesController < ApplicationController
 
   def new
     @dish = Dish.new
-    @dish.ingredients.build
+    10.times { @dish.ingredients.build }
   end
 
   def create
@@ -56,10 +56,5 @@ class DishesController < ApplicationController
 
     def set_dish
       @dish = Dish.find(params[:id])
-    end
-
-    def correct_user
-      @dish = current_user.dishes.find_by(id: params[:id])
-      redirect_to root_path if @dish.nil?
     end
 end
